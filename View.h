e8ac43ebefce445d7ab0e681e020f6c29ccfe260
CavelVellenoweth <Cavelvellenoweth@hotmail.co.uk>
2017-05-03 22:19:28 +0100

    Create View.h

---
+++ View.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <vector>
+#include <iostream>
+#include <string>
+using namespace std;
+class View
+{
+public:
+	void displayVector(vector<string>& vec);
+	void writeline(string message);
+};
